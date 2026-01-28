fe4a254770087932eb6d6065a5e8405427f16081
Sadanand09 <sadanandgurude9@gmail.com>
2026-01-28 08:31:12 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,41 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
 
-import { ENV } from './config/env.js';
-import { connectDB } from './config/db.js';
+import { inngest, functions } from "./config/inngest.js";
+
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
+app.use(express.json());
 app.use(clerkMiddleware()); //add auth object under the req => req.auth
 
+app.use("/api/inngest", serve({ client: inngest, functions: functions }));
+
 app.get("/api/health", (req, res) => {
-    res.status(200).json({ message: "Success" });
+  res.status(200).json({ message: "Success" });
 });
 
 //make your app ready for deployment
-if (ENV.NODE_ENV === "production") { 
-    app.use(express.static(path.join(__dirname, "../admin/dist")));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
-    });
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
 }
 
 const startServer = async () => {
-    await connectDB();
+  await connectDB();
 
-    app.listen(ENV.PORT, () => {
-        console.log(`🚀 Server running on PORT: ${ENV.PORT}`);
-    });
+  app.listen(ENV.PORT, () => {
+    console.log(`🚀 Server running on PORT: ${ENV.PORT}`);
+  });
 };
 
 startServer();
